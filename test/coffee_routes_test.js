be3a7bdf490398abe256055fc7199a06e0572e62
Aaron Filson <aaronfilson@gmail.com>
2016-01-26 11:29:29 -0800

    DELETE test now passes.

--- test/coffee_routes_test.js
+++ test/coffee_routes_test.js
@@ -56,6 +56,8 @@ describe('Coffee API', () => {
     });
 
     it('should be able to delete coffee info', (done) => {
+      console.log('testCoffee is : ');
+      console.dir(this.testCoffee);
       request(serverUrl)
         .delete('/' + this.testCoffee._id)
         .end((err, res) => {
